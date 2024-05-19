3c9c16a2dae62901ef5b27fc741efe25dd1574ac
axl <axescalada@gmail.com>
2024-05-19 11:55:10 -0300

    remove catpuccin settings config due problems with flakes config, comment copilot imports

--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -81,41 +81,41 @@ in {
     };
     catppuccin = {
       enable = false;
-      settings = {
-        background = {
-          light = "macchiato";
-          dark = "mocha";
+      background = {
+        light = "macchiato";
+        dark = "mocha";
+      };
+      flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+      disableBold = false;
+      disableItalic = false;
+      disableUnderline = false;
+      transparentBackground = true;
+      integrations = {
+        cmp = true;
+        noice = true;
+        notify = true;
+        neotree = true;
+        harpoon = true;
+        gitsigns = true;
+        which_key = true;
+        illuminate = {
+          enabled = true;
         };
-        flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
-        disableBold = false;
-        disableItalic = false;
-        disableUnderline = false;
-        transparentBackground = true;
-        integrations = {
-          cmp = true;
-          noice = true;
-          notify = true;
-          gitsigns = true;
-          which_key = true;
-          illuminate = {
-            enabled = true;
+        treesitter = true;
+        treesitter_context = true;
+        telescope.enabled = true;
+        indent_blankline.enabled = true;
+        mini.enabled = true;
+        native_lsp = {
+          enabled = true;
+          inlay_hints = {
+            background = true;
           };
-          treesitter = true;
-          treesitter_context = true;
-          telescope.enabled = true;
-          indent_blankline.enabled = true;
-          mini.enabled = true;
-          native_lsp = {
-            enabled = true;
-            inlay_hints = {
-              background = true;
-            };
-            underlines = {
-              errors = ["underline"];
-              hints = ["underline"];
-              information = ["underline"];
-              warnings = ["underline"];
-            };
+          underlines = {
+            errors = ["underline"];
+            hints = ["underline"];
+            information = ["underline"];
+            warnings = ["underline"];
           };
         };
       };
