042d2b01c8881c6fc361b583a6531267340a7dc4
axlEscalada <axescalada@gmail.com>
2025-08-04 09:51:20 -0300

    fix indent extra config

--- config/plugins/indent/default.nix
+++ config/plugins/indent/default.nix
@@ -21,11 +21,12 @@
     };
   };
 
-  extraConfigLua = ''
-    local hooks = require("ibl.hooks")
-    hooks.register(
-      hooks.type.WHITESPACE,
-      hooks.builtin.hide_first_space_indent_level
-    )
-  '';
+  # disable indent plugin
+  # extraConfigLua = ''
+  #   local hooks = require("ibl.hooks")
+  #   hooks.register(
+  #     hooks.type.WHITESPACE,
+  #     hooks.builtin.hide_first_space_indent_level
+  #   )
+  # '';
 }
